9b2b5f7dd8d99abc61dffe4bd5d2eef51b366f6c
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2021-12-12 18:35:36 -0300

    retorna 'done' quando o usuario consegue se autenticar

--- __tests__/User.test.js
+++ __tests__/User.test.js
@@ -1,5 +1,5 @@
 class User {
-    constructor(name, email, password) {
+    constructor({ name, email, password }) {
         this.name = name;
         this.email = email;
         this.password = password;
@@ -11,8 +11,17 @@ class UserSQLiteRepository {
         return "done";
     }
 
+    findUser(user) {
+        if (user === undefined) return undefined;
+
+        if (user.password === undefined) {
+            return { password: "wrongPassword" };
+        }
+        return user;
+    }
+
     findByEmail({ email }) {
-        return email;
+        return email === false ? true : false;
     }
 }
 
@@ -28,31 +37,72 @@ class UserUseCases {
     async create(user) {
         let userRepository = new UserRepository();
 
-        const userAlreadyExist = await userRepository.findByEmail(user);
+        const userAlreadyExists = await userRepository.findByEmail(user);
 
-        if (!userAlreadyExist) {
+        if (userAlreadyExists) {
             return { status: "not done" };
         }
         const status = await userRepository.addUser(user);
 
         return { status };
     }
+
+    async authenticate(user) {
+        let userRepository = new UserRepository();
+
+        const userExists = await userRepository.findUser(user);
+
+        if (!userExists) {
+            return { status: "not done" };
+        }
+
+        if (user.password != userExists.password) {
+            return { status: "not done" };
+        }
+
+        return { status: "done" };
+    }
 }
 
-describe("User's tests", () => {
+describe("User's create tests", () => {
     it("Should return status 'done' when create a new user ", async () => {
-        let user = new User("name", "email@gmail.com", "123456");
+        let newUser = {
+            name: "name",
+            email: "email@gmail.com",
+            password: "123456",
+        };
+        let user = new User(newUser);
 
         let userCase = new UserUseCases();
 
         expect((await userCase.create(user)).status).toBe("done");
     });
 
-    it("Should return status 'not done' when the user already exists ", async () => {
-        let user = new User("name", false, "123456");
+    it("Should return status 'not done' when the user already existss ", async () => {
+        let newUser = {
+            name: "name",
+            email: false,
+            password: "123456",
+        };
+        let user = new User(newUser);
 
         let userCase = new UserUseCases();
 
         expect((await userCase.create(user)).status).toBe("not done");
+    });
+});
+
+describe("User's authenticate tests", () => {
+    it("Should return status 'done' when the user is able to authenticate", async () => {
+        let newUser = {
+            name: undefined,
+            email: "email@gmail.com",
+            password: "123456",
+        };
+        let user = new User(newUser);
+
+        let userCase = new UserUseCases();
+
+        expect((await userCase.authenticate(user)).status).toBe("done");
     });
 });
