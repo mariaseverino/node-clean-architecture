52122839e1b9b3ef0697965ffe80a23be6b1346b
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2021-12-12 12:43:07 -0300

    Retorna status 'not done' quando usuario já existe

--- __tests__/User.test.js
+++ __tests__/User.test.js
@@ -10,6 +10,10 @@ class UserSQLiteRepository {
     addUser({ name, email, password }) {
         return "done";
     }
+
+    findByEmail({ email }) {
+        return email;
+    }
 }
 
 class UserMongoBDRepository {
@@ -24,17 +28,31 @@ class UserUseCases {
     async create(user) {
         let userRepository = new UserRepository();
 
+        const userAlreadyExist = await userRepository.findByEmail(user);
+
+        if (!userAlreadyExist) {
+            return { status: "not done" };
+        }
         const status = await userRepository.addUser(user);
 
         return { status };
     }
 }
+
 describe("User's tests", () => {
-    it("Should create a new user ", async () => {
-        let user = new User("maria", "email@gmail.com", "123456");
+    it("Should return status 'done' when create a new user ", async () => {
+        let user = new User("name", "email@gmail.com", "123456");
 
         let userCase = new UserUseCases();
 
         expect((await userCase.create(user)).status).toBe("done");
+    });
+
+    it("Should return status 'not done' when the user already exists ", async () => {
+        let user = new User("name", false, "123456");
+
+        let userCase = new UserUseCases();
+
+        expect((await userCase.create(user)).status).toBe("not done");
     });
 });
