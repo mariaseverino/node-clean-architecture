7cc4268c498c16711be32e15c50836efb558e96e
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2021-12-14 21:14:38 -0300

    Testes atualizados

--- __tests__/User.test.js
+++ __tests__/User.test.js
@@ -1,66 +1,72 @@
 const User = require("../src/Domain/Entities/User");
-const UserUseCases = require("../src/Application/UseCase/UserCreateUserCase");
+const UserUseCases = require("../src/Application/UseCase/UserUseCases");
 
 describe("User's create tests", () => {
-    it("Should return status 'done' when create a new user ", async () => {
+    it("Should return done 'done' when create a new user ", async () => {
         let newUser = {
-            name: "name",
-            email: "email@gmail.com",
+            name: "name test",
+            email: "email.test@gmail.com",
             password: "123456",
         };
         let user = new User(newUser);
 
         let userCase = new UserUseCases();
 
-        expect((await userCase.create(user)).status).toBe("done");
+        expect((await userCase.create(user)).done).toBe(true);
     });
 
-    it("Should return status 'not done' when the user already existss ", async () => {
+    it("Should return done 'false' when the user already existss ", async () => {
         let newUser = {
-            name: "name",
-            email: false,
+            name: "name test",
+            email: "email.test@gmail.com",
             password: "123456",
         };
         let user = new User(newUser);
 
         let userCase = new UserUseCases();
 
-        expect((await userCase.create(user)).status).toBe("not done");
+        expect((await userCase.create(user)).done).toBe(false);
     });
 });
 
 describe("User's authenticate tests", () => {
-    it("Should return status 'done' when the user is able to authenticate", async () => {
+    it("Should return done 'true' when the user is able to authenticate", async () => {
         let newUser = {
             name: undefined,
-            email: "email@gmail.com",
+            email: "email.test@gmail.com",
             password: "123456",
         };
         let user = new User(newUser);
 
         let userCase = new UserUseCases();
 
-        expect((await userCase.authenticate(user)).status).toBe("done");
+        expect((await userCase.authenticate(user)).done).toBe(true);
     });
 
-    it("Should return status 'not done' when the user's email is not correct", async () => {
-        let user = undefined;
+    it("Should return done 'false' when the user's email is not correct", async () => {
+        let newUser = {
+            name: "name test",
+            email: "email.test1@gmail.com",
+            password: "123456",
+        };
+
+        let user = new User(newUser);
 
         let userCase = new UserUseCases();
 
-        expect((await userCase.authenticate(user)).status).toBe("not done");
+        expect((await userCase.authenticate(user)).done).toBe(false);
     });
 
-    it("Should return status 'not done' when the user's password is not correct", async () => {
+    it("Should return done 'false' when the user's password is not correct", async () => {
         let newUser = {
-            name: undefined,
-            email: "email@gmail.com",
-            password: undefined,
+            name: "name test",
+            email: "email.test@gmail.com",
+            password: "1234568",
         };
         let user = new User(newUser);
 
         let userCase = new UserUseCases();
 
-        expect((await userCase.authenticate(user)).status).toBe("not done");
+        expect((await userCase.authenticate(user)).done).toBe(false);
     });
 });
